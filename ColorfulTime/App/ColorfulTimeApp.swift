260be2912b368bcf9b421e7d6203a1a88b9d311d
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-08-26 19:59:22 -0600

    Initial Commit

---
+++ ColorfulTime/App/ColorfulTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorfulTimeApp.swift
+//  ColorfulTime
+//
+//  Created by Montserrat Gomez on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorfulTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
