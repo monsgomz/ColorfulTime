e7c42479dead076ff81ae22d3bed86040c0f328e
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-08-29 13:51:07 -0600

    add convertion logic

--- ColorfulTime/ContentView.swift
+++ ColorfulTime/ContentView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var timeRemaining: TimeInterval = 10 //TimeInterval es representacion de segundos
+	@State private var timeRemaining: TimeInterval = 1500 //TimeInterval es representacion de segundos
 	@State private var timer: Timer?
 	@State private var isRunning: Bool = false
-	
+	@State private var setMinutes: String = "00"
+	@State private var setSeg: String = "00"
 	
     var body: some View {
-        VStack {
+		VStack (alignment: .center){
            Text("Timer")
 				.font(.title)
+			
+			HStack{
+				
+				Spacer()
+				TextField("Valor timer", text: $setMinutes)
+					.frame(width: 40, height: 15, alignment: .center)
+				Text(":")
+				TextField("Valor timer", text: $setSeg)
+					.frame(width: 40, height: 15, alignment: .center)
+				Spacer()
+			}
 			
 			Text(formattedTime())
 			
@@ -24,18 +36,30 @@ struct ContentView: View {
 				.onTapGesture {
 					startTimer()
 				}
+			
+			Image(systemName: "stop.circle")
+				.onTapGesture {
+					stopTimer()
+				}
 
         }
         .padding()
     }
 	
 	private func formattedTime() -> String { //Para mostrarlo en un texto
-		var minutes = Int(timeRemaining) / 60
-		var sec = Int(timeRemaining) % 60
+		let minutes = Int(timeRemaining) / 60
+		let sec = Int(timeRemaining) % 60
 		return String(format: "%02d:%02d", minutes, sec)
 	}
 	
 	private func startTimer() {
+		
+		let time = "\(setMinutes):\(setSeg)"
+		
+		timeRemaining = TimeInterval(minutesAndSeconds: time) ?? 0.0
+		
+//		timeRemaining = (TimeInterval(setMinutes) ?? 0.0) * 60 //convertir a minutos
+		
 		timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
 			if timeRemaining > 0 {
 				timeRemaining -= 1
@@ -49,7 +73,7 @@ struct ContentView: View {
 	private func stopTimer() {
 		isRunning = false
 		timer?.invalidate()
-		timeRemaining = 10
+		timeRemaining = 1500
 	}
 }
 
---
+++ ColorfulTime/Helpers/ExtensionTimeInterval.swift
@@ -0,0 +1,21 @@
+//
+//  ExtensionTimeInterval.swift
+//  ColorfulTime
+//
+//  Created by Montserrat Gomez on 29/08/24.
+//
+
+import Foundation
+
+extension TimeInterval {
+	init?(minutesAndSeconds: String) {
+		let components = minutesAndSeconds.split(separator: ":")
+		guard components.count == 2,
+			  let minutes = Double(components[0]),
+			  let seconds = Double(components[1]) else {
+			return nil
+		}
+		
+		self = (minutes * 60) + seconds
+	}
+}
